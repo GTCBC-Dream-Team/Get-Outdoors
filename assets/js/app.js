81f3af3329e3297490b3a6003b9a80cb8bf3e819
Gene Williams <GeneWillb@Gmail.com>
2017-09-28 21:10:55 -0400

    geoloation works

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,29 +8,19 @@
 
 // 'http://maps.googleapis.com/maps/api/geocode/json'
 
-function initMap() {
-    var map = new google.maps.Map(document.getElementById('map'), {
-        zoom: 8,
-        center: {lat: 33.748995, lng: -84.387982}
-    });
-    var geocoder = new google.maps.Geocoder();
-    
-    document.getElementById('submit').addEventListener('click', function() {
-        geocodeAddress(geocoder, map);
-    });
-}
+const key = "AIzaSyBWhgudH2tEdR71f3K0SmIUysNLNupoicI";
 
-function geocodeAddress(geocoder, resultsMap) {
-    var address = document.getElementById('address').value;
-    geocoder.geocode({'address': address}, function(results, status) {
-        if (status === 'OK') {
-            resultsMap.setCenter(results[0].geometry.location);
-            var marker = new google.maps.Marker({
-                map: resultsMap,
-                position: results[0].geometry.location
-            });
-        } else {
-            alert('Geocode was not successful for the following reason: ' + status);
-        }
-    });
-}
+
+
+$("#submit").on("click", function() {
+    let address = $("#address").val();
+    const queryURL = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}&key=${key}`;
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).done(function (response) {
+        console.log(response.results[0].geometry.location.lat);
+        console.log(response.results[0].geometry.location.lng);
+    })
+});
+
