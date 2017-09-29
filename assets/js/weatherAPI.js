76bde7c61b218943110f38ebf78787029023664d
Katie Lejman <misskatiemary@gmail.com>
2017-09-29 15:12:22 -0400

    Merge

--- assets/js/weatherAPI.js
+++ assets/js/weatherAPI.js
@@ -1,7 +1,8 @@
-
+var locationName = "36.176346,-95.938979";
 $.ajax({
-    url:"https://gtproxy2.herokuapp.com/api/metaweather/location/search/?query=san",
+    url:"https://gtproxy2.herokuapp.com/api/metaweather/location/search/?lattlong=" + locationName,
     method: "GET"
 }).done(function(response){
+    console.log(locationName);
     console.log(response);
 });
