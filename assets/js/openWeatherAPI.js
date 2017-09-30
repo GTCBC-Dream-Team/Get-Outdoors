279c79f718b5a3c462947597a4d4b0d0f648cd61
jwilson99 <juliewilsongt@gmail.com>
2017-09-30 13:55:14 -0400

    added current weather data to today's weather scroller

--- assets/js/openWeatherAPI.js
+++ assets/js/openWeatherAPI.js
@@ -11,24 +11,39 @@ $.ajax({
     method: "GET"
 }).done(function(response){
     console.log(response);
+
     //shows current weather state
     var currentWeatherState = response.weather[0].main;
     console.log("current weather state: " + currentWeatherState);
+    $("#currentWeatherHead").html("Today's Weather");
+    $("#currentWeatherBody").html(currentWeatherState + "<br>");
+
     //shows current temperature
     var tempKelvin = response.main.temp;
     var tempFahrenheit = (tempKelvin - 273.15)*1.8000;
     tempFahrenheit = Number(Math.round(tempFahrenheit+'e1')+'e-1');
     console.log("current temperature: " + tempFahrenheit + "F");
+    $("#currentWeatherBody").append("Current: " + tempFahrenheit + "<br>");
+
     //shows today's min temperature
     var minKelvin = response.main.temp_min;
     var minFahrenheit = (minKelvin - 273.15)*1.8000;
     minFahrenheit = Number(Math.round(minFahrenheit+'e1')+'e-1');
     console.log("min temperature: " + minFahrenheit + "F");
+    $("#currentWeatherBody").append("Min: " + minFahrenheit + "<br>");
+
     //shows today's max temperature
     var maxKelvin = response.main.temp_max;
     var maxFahrenheit = (maxKelvin - 273.15)*1.8000;
     maxFahrenheit = Number(Math.round(maxFahrenheit+'e1')+'e-1');
     console.log("max temperature: " + maxFahrenheit + "F");
+    $("#currentWeatherBody").append("Max: " + maxFahrenheit + "<br>");
+
+    //shows today's sunrise time
+
+    //show today's sunset time
+
+    //show today's day length
 });
 
 //call to weather API future weather forecast
