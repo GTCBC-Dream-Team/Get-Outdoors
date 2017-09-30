7bbb54973afbb73357d09ce0859074e376f6b357
Gene Williams <GeneWillb@Gmail.com>
2017-09-30 14:12:21 -0400

    place markers show up

--- assets/js/app.js
+++ assets/js/app.js
@@ -129,7 +129,18 @@ function callback(results, status) {
     }
 }
 
-
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+    
+    google.maps.event.addListener(marker, 'click', function() {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}   
 // function hey(name) {
 //     return function () {
 //         console.log('Hey', name);
